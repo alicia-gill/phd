9e0ca5f6497b4297f79f8ee0f53e3fa32ea096df
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2022-08-02 18:09:28 +0100

    Removed dt

--- R/prevalence.R
+++ R/prevalence.R
@@ -1,6 +1,6 @@
-prevalence <- function(epidemic, stop_time, dt) {
-  t <- seq(0,stop_time,by=dt)
-  prev <- list("day" = t / dt, "prev" = 0)
+prevalence <- function(epidemic, stop_time) {
+  t <- seq(0,stop_time,by=1)
+  prev <- list("day" = t, "prev" = 0)
   l <- length(t)
   for (i in 1:l) {
     ti <- t[i]
--- R/skellam_llik.R
+++ R/skellam_llik.R
@@ -1,17 +1,17 @@
-skellam_llik <- function(birth_rate, death_rate, dt, prevalence, log=T) {
-  b <- birth_rate*dt
-  d <- death_rate*dt
+skellam_llik <- function(birth_rate, death_rate, prevalence, log=T) {
+  b <- birth_rate
+  d <- death_rate
   prev <- prevalence[,2]
 
   n <- length(prev)
   I <- 0
-  root <- sqrt(b*d)
+  root <- sqrt(birth_rate*death_rate)
   for (i in 2:n) {
     previ <- prev[i-1]
     I <- I + log(besselI(x=2*previ*root,nu=abs(prev[i]-previ),expon.scaled=T)) + 2*previ*root
   }
 
-  logl <- -(b+d)*(sum(prev)-prev[n]) + ((prev[n]-prev[1])/2)*(log(b)-log(d)) + I
+  logl <- -(birth_rate+d)*(sum(prev)-prev[n]) + ((prev[n]-prev[1])/2)*(log(birth_rate)-log(death_rate)) + I
 
   if (log==T) {
     return(logl)
