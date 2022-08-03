c8a96bff83f0bd7bb0948b65da09994b12cb6de6
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2022-08-03 15:40:39 +0100

    updated smooth.R

--- R/smooth.R
+++ R/smooth.R
@@ -4,10 +4,11 @@ smooth <- function(noisy_prevalence, proportion_obs) {
 
   smooth <- list("day" = 0:n_days, "prev" = 1)
 
-  for (i in 2:n_days) {
-    smooth$prev[i] <- round(sum(prev[(i-1):(i+1)]/proportion_obs)/3)
+  smooth$prev[2] <- floor(prev[2]/proportion_obs)
+  for (i in 3:n_days) {
+    smooth$prev[i] <- round(sum(floor(prev[(i-1):(i+1)]/proportion_obs)/3))
   }
-  smooth$prev[n_days+1] <- prev[n_days+1]
+  smooth$prev[n_days+1] <- floor(prev[n_days+1]/proportion_obs)
 
   smooth <- as.data.frame(smooth)
   return(smooth)
