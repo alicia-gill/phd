0811f0bcb28915f7f83a36156ed7dd8176fd6708
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2022-08-03 15:38:59 +0100

    add pm_noisy.R

---
+++ R/pm_noisy.R
@@ -0,0 +1,69 @@
+#noisy epidemic
+pm_noisy <- function(iter, birth_rate_0, max_birth_rate=100, prevalence_0, death_rate, ptree, noisy_prevalence, proportion_obs, n_particles) {
+  sys_time <- as.numeric(Sys.time())
+
+  n <- nrow(noisy_prevalence)
+  stop_time <- n - 1
+
+  output <- list("birth_rate" = birth_rate_0, "acceptance_rate" = 0, "run_time" = 0)
+  n_accepted <- 0
+
+  b_old <- birth_rate_0
+  prev_old <- prevalence_0
+
+  #prior is uniform
+  f_hat_old <- skellam_llik(birth_rate = b_old, death_rate = death_rate, prevalence = prev_old) +
+               genetic_llik(birth_rate = b_old, ptree = ptree, prevalence = prev_old, stop_time = stop_time) +
+               sum(dbinom(x = noisy_prevalence[-1,2], size = prev_old[-1,2], prob = proportion_obs, log = TRUE))
+
+  smooth_prev <- smooth(noisy_prevalence = noisy_prevalence, proportion_obs = proportion_obs)
+  lambda <- 0.1 + smooth_prev[-1,2]
+  q_old <- sum(extraDistr::dtpois(prev_old[-1,2] - smooth_prev[-1,2] + round(lambda), lambda = lambda, a = noisy_prevalence[-1,2] + round(lambda) - smooth_prev[-1,2] - 1, log = TRUE))
+
+  for (i in 1:iter) {
+    j <- 100*i/iter
+    if (j %% 1 == 0) {
+      print(paste0(j,"%"))
+    }
+
+    #step 1: sample b_new and sample prev_new
+    eps <- rnorm(1,0,0.01)
+    b_new <- b_old + eps
+    #if proposal is negative or larger than max_birth_rate, then bounce back
+    while (b_new < 0 | b_new > max_birth_rate) {
+      if (b_new < 0) {
+        b_new <- -b_new
+      }
+      if (b_new > max_birth_rate) {
+        b_new <- max_birth_rate - b_new
+      }
+    }
+
+    #step 2: compute likelihood
+    f_hat_q <- unlist(lapply(1:n_particles, propose_pois, noisy_prevalence = noisy_prevalence, proportion_obs = proportion_obs, birth_rate = b_new, death_rate = death_rate, ptree = ptree))
+    f_hat_log <- f_hat_q[seq(1,n_particles,by=2)]
+    q_log <- f_hat_q[seq(2,n_particles,by=2)]
+    f_hat_new <- matrixStats::logSumExp(f_hat_log) - log(n_particles)
+    q_new <- matrixStats::logSumExp(q_log) - log(n_particles)
+
+    #step 3: compute acceptance probability
+    logr <- f_hat_new - q_new - f_hat_old + q_old
+    loga <- min(0,logr)
+    a <- exp(loga)
+
+    #step 4: accept/reject
+    u <- runif(1,0,1)
+    if (u <= a) {
+      n_accepted <- n_accepted + 1
+      b_old <- b_new
+      f_hat_old <- f_hat_new
+      q_old <- q_new
+    }
+    output$birth_rate[i] <- b_old
+  }
+
+  output$acceptance_rate <- n_accepted/iter
+  output$run_time <- as.numeric(Sys.time()) - sys_time
+  return(output)
+}
+
