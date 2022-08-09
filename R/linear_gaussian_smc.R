e2de5b84abe4f404d366fe45961a2ba7bf4e700a
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2022-08-09 14:25:47 +0100

    added integrated likelihood to smc

--- R/linear_gaussian_smc.R
+++ R/linear_gaussian_smc.R
@@ -26,7 +26,7 @@ for (i in 1:t) {
 
 plot(1:t, x, type="o", pch=20, ylim=c(min(x,y),max(x,y)))
 lines(1:t, y, type="o", pch=20, col="red")
-legend("topright", legend=c("True X", "Observed Y", "KF", "SMC"), col=c("Black", "Red", "Blue", "Green"), lty=1)
+legend("topleft", legend=c("True X", "Observed Y", "KF", "SMC"), col=c("Black", "Red", "Blue", "Green"), lty=1)
 
 library(dlm)
 mod <- dlm(FF=FF, V=V, GG=GG, W=W, m0=m0, C0=C0)
@@ -37,10 +37,12 @@ N <- 1000
 X <- matrix(nrow=N, ncol=t)
 X[,1] <- rnorm(N, FF*y[1], 1)
 w <- rep(0,N)
+int_llik <- 0
 for (j in 1:N) {
   #likelihood: y1|x1 ~ N(FF*x1,1), prior: x1 ~ N(GG*m0,1), proposal: q(x1) ~ N(GG*y1,1)
   w[j] <- dnorm(y[1], FF*X[j,1], 1) * dnorm(X[j,1], GG*m0, 1) / dnorm(X[j,1], GG*y[1], 1)
 }
+int_llik <- int_llik + log(mean(w))
 w.norm <- w/sum(w)
 X_resample <- matrix(nrow=N, ncol=t)
 X_resample[,1] <- sample(X[,1], size=N, replace=T, prob=w.norm)
@@ -50,7 +52,14 @@ for (i in 2:t) {
     #likelihood: yi|xi~N(FF*xi,1), xi|xi-1~N(GG*xi-1,1), q(xi)~N(GG*yi,1)
     w[j] <- dnorm(y[i], FF*X[j,i], 1) * dnorm(X[j,i], GG*X_resample[j,i-1], 1) / dnorm(X[j,i], GG*y[i], 1)
   }
+  int_llik <- int_llik + log(mean(w))
   w.norm <- w/sum(w)
   X_resample[,i] <- sample(X[,i], size=N, replace=T, prob=w.norm)
 }
+print(int_llik)
+
 lines(1:t, apply(X_resample,2,mean),type="o",pch=20,col="green")
+
+#find smc package and compare likelihoods
+#implement into mh
+#infer one parameter (FF,GG=1, infer variance of unobserved process)
