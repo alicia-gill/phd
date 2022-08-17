fbfa0430d4e57202251bcf2eb058e8c0a5351174
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2022-08-17 14:37:02 +0100

    added roxygen documentation

--- R/prevalence.R
+++ R/prevalence.R
@@ -1,3 +1,15 @@
+#' Prevalence
+#'
+#' Turns simulated epidemic into a vector of cases per day.
+#'
+#' @param epidemic The output from epidemic.R.
+#' @param stop_time The number of days the epidemic simulation was run for.
+#'
+#' @return A data frame with 2 columns and N+1 rows. Column 1 is day and column 2 is number of cases.
+#' @export
+#'
+#' @examples
+#' prevalence(epidemic = epi, stop_time = 50)
 prevalence <- function(epidemic, stop_time) {
   t <- seq(0,stop_time,by=1)
   prev <- list("day" = t, "prev" = 0)
