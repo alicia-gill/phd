8bbd2e096963f091a59478c4359f816ec1c1caa9
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2022-08-22 14:00:58 +0100

    truncation should be strict

--- R/propose_pois.R
+++ R/propose_pois.R
@@ -22,7 +22,7 @@ propose_pois <- function(noisy_prevalence, proportion_obs, birth_rate, death_rat
 
   prev_new <- rep(1, n_days + 1)
   prev_new[-1] <- smooth_prev[-1,2] +
-                  extraDistr::rtpois(n_days, lambda = lambda, a = noisy_prevalence[-1,2] + round(lambda) - smooth_prev[-1,2] - 1) -
+                  extraDistr::rtpois(n_days, lambda = lambda, a = noisy_prevalence[-1,2] + round(lambda) - smooth_prev[-1,2]) -
                   round(lambda)
 
   prev_new <- data.frame("day" = 0:n_days, "prev" = prev_new)
@@ -30,7 +30,7 @@ propose_pois <- function(noisy_prevalence, proportion_obs, birth_rate, death_rat
   f_hat <- skellam_llik(birth_rate = birth_rate, death_rate = death_rate, prevalence = prev_new) +
            genetic_llik(birth_rate = birth_rate, ptree = ptree, prevalence = prev_new, stop_time = n_days) +
            sum(dbinom(x = noisy_prevalence[-1,2], size = prev_new[-1,2], prob = proportion_obs, log = TRUE))
-  q <- sum(extraDistr::dtpois(prev_new[-1,2] - smooth_prev[-1,2] + round(lambda), lambda = lambda, a = noisy_prevalence[-1,2] + round(lambda) - smooth_prev[-1,2] - 1, log = TRUE))
+  q <- sum(extraDistr::dtpois(prev_new[-1,2] - smooth_prev[-1,2] + round(lambda), lambda = lambda, a = noisy_prevalence[-1,2] + round(lambda) - smooth_prev[-1,2], log = TRUE))
 
   return(list("f_hat"=f_hat, "q"=q))
 }
