d4d7c64ffc66bc9b57b895088627b9efc9e37d9d
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2023-01-23 19:17:32 +0000

    function to do systematic resampling

--- R/systematic_sample.R
+++ R/systematic_sample.R
@@ -1,9 +1,18 @@
+#' Systematic Resampling
+#'
+#' Implements systematic resampling.
+#'
+#' @param n_particles number of particles used in the sampling.
+#' @param norm_weights normalised weights of the particles.
+#'
+#' @return resampled particles
+#' @export
+#'
+#' @examples
+#' systematic_sample <- function(n_particles = 1000, norm_weights = rep(1/1000, 1000))
 systematic_sample <- function(n_particles, norm_weights) {
-  u <- rep(NA, n_particles)
-  u[1] <- runif(1, 0, 1/n_particles)
-  for (i in 2:n_particles) {
-    u[i] <- u[1] + (i-1)/n_particles
-  }
+  u1 <- runif(1, 0, 1/n_particles)
+  u <- (0:(n_particles-1))/n_particles + u1
   cumulative_sum <- cumsum(norm_weights)
   resample <- rep(NA, n_particles)
   i <- 1
