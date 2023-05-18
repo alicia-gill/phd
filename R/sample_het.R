513de4538bb53975a8e8e9a6e3e3c23a95d0e1f2
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2023-05-18 11:55:37 +0100

    samples from all leaves, not only present day ones

---
+++ R/sample_het.R
@@ -0,0 +1,37 @@
+#' Sample from Infected
+#'
+#' Samples and keeps leaves with probability pi.
+#'
+#' @param ptree object of class phylo.
+#' @param pi probability of sampling a leaf.
+#' @param day vector of days to sample from.
+#'
+#' @return object of class phylo; the tree keeping only the sampled leaves.
+#' @export
+#'
+#' @examples
+#' sample_het(ptree = full_tree, pi = 0.1)
+sample_het <- function(ptree, pi) {
+  if (pi < 0 | pi > 1) {
+    warning("pi must be between 0 and 1")
+    break
+  }
+
+  #number of leaves
+  n_leaves <- length(ptree$tip.label)
+
+  #distance from root node (always coded as n_leaves+1) and leaves (always codes from 1 to n_leaves)
+  distance <- ape::dist.nodes(ptree)[1:n_leaves, n_leaves+1]
+
+  max <- max(distance)
+  change <- (max - distance) %% 1
+
+  edges <- which(ptree$edge[,2] <= n_leaves)
+  ptree$edge.length[edges] <- ptree$edge.length[edges] + change
+
+  u <- runif(n_leaves)
+  keep <- (1:n_leaves)[u <= pi]
+
+  new_tree <- ape::keep.tip(ptree, keep)
+  return(new_tree)
+}
