cbf4d7a5612c6e9e037ca83d86819a3e79887b5b
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2023-05-22 18:32:44 +0100

    probability of sampling leaves at present day and past separate

--- R/sample_het.R
+++ R/sample_het.R
@@ -3,16 +3,16 @@
 #' Samples and keeps leaves with probability pi.
 #'
 #' @param ptree object of class phylo.
-#' @param pi probability of sampling a leaf.
-#' @param day vector of days to sample from.
+#' @param pi0 probability of sampling a leaf at the present day.
+#' @param pi1 probability of sampling a leaf before the present day.
 #'
 #' @return object of class phylo; the tree keeping only the sampled leaves.
 #' @export
 #'
 #' @examples
-#' sample_het(ptree = full_tree, pi = 0.1)
-sample_het <- function(ptree, pi) {
-  if (pi < 0 | pi > 1) {
+#' sample_het(ptree = full_tree, pi0 = 0, pi1 = 0.01)
+sample_het <- function(ptree, pi0, pi1) {
+  if (pi0 < 0 | pi0 > 1 | pi1 < 0 | pi1 > 1) {
     warning("pi must be between 0 and 1")
     break
   }
@@ -31,9 +31,19 @@ sample_het <- function(ptree, pi) {
   edges <- which(ptree$edge[,2] <= n_leaves)
   ptree$edge.length[edges] <- ptree$edge.length[edges] + change
 
+  distance <- ape::dist.nodes(ptree)[1:n_leaves, n_leaves+1]
+  max <- max(distance)
+
+  #option 1 - split into present/not present and sample separately
+  #label leaves
+  day1 <- (1:n_leaves)[which(distance < max)]
+  day0 <- (1:n_leaves)[-day1]
   #sample leaves
-  u <- runif(n_leaves)
-  keep <- (1:n_leaves)[u <= pi]
+  u0 <- runif(length(day0))
+  keep0 <- day0[u0 <= pi0]
+  u1 <- runif(length(day1))
+  keep1 <- day1[u1 <= pi1]
+  keep <- c(keep0, keep1)
 
   new_tree <- ape::keep.tip(ptree, keep)
   return(new_tree)
