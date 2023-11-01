4458e582660e4839a908ebafc8c1308ea00eb651
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2023-11-01 20:57:46 +0000

    change axis labels on plot fns

--- R/plot_fns.R
+++ R/plot_fns.R
@@ -13,7 +13,7 @@ NULL
 
 #' @rdname plot_fns
 #' @export
-plot_birthrates <- function(chain, q=0.95, burn_in=0) {
+plot_birthrates <- function(chain, q=0.95, burn_in=0, start_time=NA, end_time=NA) {
   if (burn_in > 0) {
     birth_rates <- chain$birth_rate[-(1:burn_in),]
   } else {
@@ -23,15 +23,27 @@ plot_birthrates <- function(chain, q=0.95, burn_in=0) {
   uq <- 1-lq
   min <- min(matrixStats::colQuantiles(birth_rates, probs=lq, na.rm=T))
   max <- max(matrixStats::colQuantiles(birth_rates, probs=uq, na.rm=T))
+  ncol <- ncol(birth_rates)
+  if (is.na(start_time) & is.na(end_time)) {
+    warning("Specify start or end time")
+    break
+  }
+  if (is.na(end_time)) {
+    end_time <- start_time + ncol - 1
+  }
+  if (is.na(start_time)) {
+    start_time <- end_time - ncol + 1
+  }
   par(mar=c(4,4,1.5,1.5))
-  plot(apply(birth_rates, 2, mean, na.rm=T), type="l", ylim=c(min,max), xlab="Day", ylab="B(t)")
+  plot(apply(birth_rates, 2, mean, na.rm=T), type="l", xlim=c(1, ncol), ylim=c(min,max), xlab="Time", ylab="B(t)", xaxt="n")
+  axis(1, at=seq(0, ncol, by=10), labels=seq(start_time-1, end_time, by=10))
   lines(matrixStats::colQuantiles(birth_rates, probs=lq, na.rm=T), col="blue")
   lines(matrixStats::colQuantiles(birth_rates, probs=uq, na.rm=T), col="blue")
 }
 
 #' @rdname plot_fns
 #' @export
-plot_rt <- function(chain, death_rate, q=0.95, burn_in=0) {
+plot_rt <- function(chain, death_rate, q=0.95, burn_in=0, start_time=NA, end_time=NA) {
   if (burn_in > 0) {
     rt <- chain$birth_rate[-(1:burn_in),]/death_rate
   } else {
@@ -41,15 +53,27 @@ plot_rt <- function(chain, death_rate, q=0.95, burn_in=0) {
   uq <- 1-lq
   min <- min(matrixStats::colQuantiles(rt, probs=lq, na.rm=T))
   max <- max(matrixStats::colQuantiles(rt, probs=uq, na.rm=T))
+  ncol <- ncol(rt)
+  if (is.na(start_time) & is.na(end_time)) {
+    warning("Specify start or end time")
+    break
+  }
+  if (is.na(end_time)) {
+    end_time <- start_time + ncol - 1
+  }
+  if (is.na(start_time)) {
+    start_time <- end_time - ncol + 1
+  }
   par(mar=c(4,4,1.5,1.5))
-  plot(apply(rt, 2, mean, na.rm=T), type="l", ylim=c(min,max), xlab="Day", ylab="R(t)")
+  plot(apply(rt, 2, mean, na.rm=T), type="l", xlim=c(1, ncol), ylim=c(min,max), xlab="Time", ylab="R(t)", xaxt="n")
+  axis(1, at=seq(0, ncol, by=10), labels=seq(start_time-1, end_time, by=10))
   lines(matrixStats::colQuantiles(rt, probs=lq, na.rm=T), col="blue")
   lines(matrixStats::colQuantiles(rt, probs=uq, na.rm=T), col="blue")
 }
 
 #' @rdname plot_fns
 #' @export
-plot_prevalence <- function(chain, q=0.95, burn_in=0) {
+plot_prevalence <- function(chain, q=0.95, burn_in=0, start_time=NA, end_time=NA) {
   if (burn_in > 0) {
     prevalence <- chain$prevalence[-(1:burn_in),]
   } else {
@@ -59,9 +83,21 @@ plot_prevalence <- function(chain, q=0.95, burn_in=0) {
   uq <- 1-lq
   min <- 0
   max <- max(matrixStats::colQuantiles(prevalence, probs=uq, na.rm=T))
-  n <- ncol(prevalence)-1
+  ncol <- ncol(prevalence)
+  n <- ncol-1
+  if (is.na(start_time) & is.na(end_time)) {
+    warning("Specify start or end time")
+    break
+  }
+  if (is.na(end_time)) {
+    end_time <- start_time + ncol
+  }
+  if (is.na(start_time)) {
+    start_time <- end_time - ncol + 1
+  }
   par(mar=c(4,4,1.5,1.5))
-  plot(0:n, apply(prevalence, 2, mean, na.rm=T), type="l", ylim=c(min,max), xlab="Day", ylab="Prevalence")
+  plot(0:n, apply(prevalence, 2, mean, na.rm=T), type="l", xlim=c(0, ncol-1), ylim=c(min,max), xlab="Time", ylab="Prevalence", xaxt="n")
+  axis(1, at=seq(0, ncol, by=10), labels=seq(start_time, end_time, by=10))
   lines(0:n, matrixStats::colQuantiles(prevalence, probs=lq, na.rm=T), col="blue")
   lines(0:n, matrixStats::colQuantiles(prevalence, probs=uq, na.rm=T), col="blue")
 }
