79ddcca88932acca535708412db52e2fc7bd935a
Alicia Gill <85570585+alicia-gill@users.noreply.github.com>
2024-05-10 19:28:23 +0100

    made find_nopt more flexible on inputs

--- R/find_nopt.R
+++ R/find_nopt.R
@@ -15,7 +15,7 @@
 #'
 #' @examples
 #' find_nopt(sigma0 = 0.1, proportion_obs0 = 0.5, death_rate = 0.1, ptree = sample_tree, day = 1, noisy_prevalence = noisy_prev, print = T)
-find_nopt <- function(sigma0, proportion_obs0, x0 = 1, death_rate, ptree, day = 0, noisy_prevalence, resampling_scheme = "systematic", print = F) {
+find_nopt <- function(sigma0, proportion_obs0, x0 = 1, death_rate, ptree, day = 0, noisy_prevalence, ess_threshold_prop = 0.5, resampling_scheme = "systematic", backward_sim = F, print = F) {
   n <- nrow(noisy_prevalence)
   stop_time <- n - 1
   genetic_data <- genetic_data(ptree = ptree, stop_time = stop_time, day = day)
@@ -44,12 +44,12 @@ find_nopt <- function(sigma0, proportion_obs0, x0 = 1, death_rate, ptree, day = 
     sqrtSigma_old <- expm::sqrtm(Sigma_old)
   }
 
-  n_particles <- 2000
-  ess_threshold <- n_particles/2
+  n_particles <- 1000
+  ess_threshold <- n_particles*ess_threshold_prop
 
   prior_old <- dexp(x = sigma_old, rate = lambda_sigma, log = T) + dunif(x = p_obs_old, min = 0, max = 1, log = T)
 #  prior_old <- dexp(x = sigma_old, rate = lambda_sigma, log = T) + dbeta(x = p_obs_old, shape1 = alpha_pobs, shape2 = beta_pobs, log = T)
-  f_hat_old <- sir_mix(n_particles = n_particles, ess_threshold = ess_threshold, x0 = x0, death_rate = death_rate, sigma = sigma_old, proportion_obs = p_obs_old, noisy_prevalence = noisy_prevalence, genetic_data = genetic_data, resampling_scheme = resampling_scheme, backward_sim = F)$int_llik
+  f_hat_old <- sir_mix(n_particles = n_particles, ess_threshold = ess_threshold, x0 = x0, death_rate = death_rate, sigma = sigma_old, proportion_obs = p_obs_old, noisy_prevalence = noisy_prevalence, genetic_data = genetic_data, resampling_scheme = resampling_scheme, backward_sim = backward_sim)$int_llik
 
   #need to find a good estimate of the posterior mean for sigma and p_obs
   #first 500 for burn-in
@@ -87,7 +87,7 @@ find_nopt <- function(sigma0, proportion_obs0, x0 = 1, death_rate, ptree, day = 
 
     prior_new <- dexp(x = sigma_new, rate = lambda_sigma, log = T) + dunif(x = p_obs_new, min = 0, max = 1, log = T)
 #    prior_new <- dexp(x = sigma_new, rate = lambda_sigma, log = T) + dbeta(x = p_obs_new, shape1 = alpha_pobs, shape2 = beta_pobs, log = T)
-    f_hat_new <- sir_mix(n_particles = n_particles, ess_threshold = ess_threshold, x0 = x0, death_rate = death_rate, sigma = sigma_new, proportion_obs = p_obs_new, noisy_prevalence = noisy_prevalence, genetic_data = genetic_data, resampling_scheme = resampling_scheme, backward_sim = F)$int_llik
+    f_hat_new <- sir_mix(n_particles = n_particles, ess_threshold = ess_threshold, x0 = x0, death_rate = death_rate, sigma = sigma_new, proportion_obs = p_obs_new, noisy_prevalence = noisy_prevalence, genetic_data = genetic_data, resampling_scheme = resampling_scheme, backward_sim = backward_sim)$int_llik
     logr <- prior_new + f_hat_new - prior_old - f_hat_old
     loga <- min(0,logr)
     a <- exp(loga)
@@ -155,7 +155,7 @@ find_nopt <- function(sigma0, proportion_obs0, x0 = 1, death_rate, ptree, day = 
 
     prior_new <- dexp(x = sigma_new, rate = lambda_sigma, log = T) + dunif(x = p_obs_new, min = 0, max = 1, log = T)
 #    prior_new <- dexp(x = sigma_new, rate = lambda_sigma, log = T) + dbeta(x = p_obs_new, shape1 = alpha_pobs, shape2 = beta_pobs, log = T)
-    f_hat_new <- sir_mix(n_particles = n_particles, ess_threshold = ess_threshold, x0 = x0, death_rate = death_rate, sigma = sigma_new, proportion_obs = p_obs_new, noisy_prevalence = noisy_prevalence, genetic_data = genetic_data, resampling_scheme = resampling_scheme, backward_sim = F)$int_llik
+    f_hat_new <- sir_mix(n_particles = n_particles, ess_threshold = ess_threshold, x0 = x0, death_rate = death_rate, sigma = sigma_new, proportion_obs = p_obs_new, noisy_prevalence = noisy_prevalence, genetic_data = genetic_data, resampling_scheme = resampling_scheme, backward_sim = backward_sim)$int_llik
     logr <- prior_new + f_hat_new - prior_old - f_hat_old
     loga <- min(0,logr)
     a <- exp(loga)
@@ -196,7 +196,7 @@ find_nopt <- function(sigma0, proportion_obs0, x0 = 1, death_rate, ptree, day = 
     if (print == T) {
       print(r)
     }
-    nopt_llik[r] <- sir_mix(n_particles = Ns, sigma = sigma_mean, death_rate = death_rate, noisy_prevalence = noisy_prevalence, proportion_obs = p_obs_mean, genetic_data = genetic_data, ess_threshold = Ns/2, resampling_scheme = resampling_scheme, backward_sim = FALSE)$int_llik
+    nopt_llik[r] <- sir_mix(n_particles = Ns, sigma = sigma_mean, death_rate = death_rate, noisy_prevalence = noisy_prevalence, proportion_obs = p_obs_mean, genetic_data = genetic_data, ess_threshold = Ns*ess_threshold_prop, resampling_scheme = resampling_scheme, backward_sim = backward_sim)$int_llik
   }
   var <- sum(nopt_llik^2)/R - mean(nopt_llik)^2
   Nopt <- ceiling(Ns * (var) / (0.92^2))
